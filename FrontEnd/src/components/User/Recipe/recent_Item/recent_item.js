ea1e0ff1fafda7111b1e60faf2091253417e559d
KKuyen <22521225@gm.uit.edu.vn>
2024-09-18 14:02:09 +0700

    xong frontEnd recipe

---
+++ FrontEnd/src/components/User/Recipe/recent_Item/recent_item.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./recent_item.css";
+import { CiCalendar } from "react-icons/ci";
+
+const RecentItem = ({ tname, ttime, tlink }) => {
+  const RecentName = tname;
+  const RecentTime = ttime;
+  const RecentLink = tlink;
+  return (
+    <div className="recent">
+      <div
+        className="recent_img"
+        style={{
+          backgroundImage: `url(${tlink})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
+      <div className="recent_txt_div">
+        <p className="p1">{RecentName}</p>
+        <div className="div-ngang">
+          <CiCalendar className="calendar" />
+          <p className="p2">{RecentTime}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecentItem;
--- FrontEnd/src/components/User/Recipe/recipe.js
+++ FrontEnd/src/components/User/Recipe/recipe.js
@@ -4,6 +4,12 @@ import "./recipe.css";
 import Header from "../widget/top";
 import { LiaFilterSolid } from "react-icons/lia";
 import FilterCategory from "./filter_drop_category/filter_category";
+import { Pagination } from "antd";
+import DisplayItem from "./display_item/displayItem";
+import RecentItem from "./recent_Item/recent_item";
+const onChange = (pageNumber) => {
+  console.log("Page: ", pageNumber);
+};
 function Recipes() {
   const navItems = [{ link: "/recipes", text: "Recipes" }];
   const listCategory = [
@@ -26,7 +32,7 @@ function Recipes() {
       <main className="content">
         <div className="container">
           <div className="row ">
-            <div className="col-3">
+            <div className="col-4 cot1">
               <div className="col">
                 <div className="div1"></div>
                 <div className="div2">
@@ -42,11 +48,37 @@ function Recipes() {
                     listCategory={listCategory}
                   ></FilterCategory>
                   <hr className="line"></hr>
-                  <p>Recenty Saved</p>
+                  <p className="recenttxt">Recenty Saved</p>
+
+                  <RecentItem
+                    tname={"Smoothie xoài chuối kiwi"}
+                    ttime={"Apr 25, 2024"}
+                    tlink={
+                      "https://file.hstatic.net/1000361915/article/sinh-to-chuoi-xoai-giam-can_9ad409cb707d438689515eeb643275ea.jpg"
+                    }
+                  />
+                  <br />
+                  <RecentItem
+                    tlink={
+                      "https://file.hstatic.net/1000361915/article/sinh-to-chuoi-xoai-giam-can_9ad409cb707d438689515eeb643275ea.jpg"
+                    }
+                    tname={"Smoothie xoài chuối kiwi trộn dâu tây Đà Lạt"}
+                    ttime={"Apr 25, 2024"}
+                  />
+                  <br />
+                  <RecentItem
+                    tlink={
+                      "https://file.hstatic.net/1000361915/article/sinh-to-chuoi-xoai-giam-can_9ad409cb707d438689515eeb643275ea.jpg"
+                    }
+                    tname={"Smoothie xoài chuối kiwi"}
+                    ttime={"Apr 25, 2024"}
+                  />
                 </div>
               </div>
             </div>
-            <div className="col">
+            <div className="space"></div>
+
+            <div className="col cot2">
               <div className="div1">
                 <div className="search">
                   <input
@@ -66,15 +98,134 @@ function Recipes() {
                   <p className="txt_Sortby"> Sort by:</p>
                   <select className="sort-by">
                     <option value="latest"> Latest </option>
-                    <option value="best-seller"> Best Seller </option>
-                    <option value="best-discount"> Hot Deal </option>
-                    <option value="best-discount"> Rate </option>
+                    <option value="best-seller"> View </option>
                   </select>
                 </div>
                 <p className="txt2">
                   <span className="txt-bold txt2">52</span> Results Found
                 </p>
               </div>
+              <div className="display_row">
+                <div className="display_item1">
+                  <DisplayItem
+                    istrue={true}
+                    ttime={140}
+                    ttag={"Vietnamese Food"}
+                    tby={"Admin"}
+                    tcomments={65}
+                    tname={"Trứng cuộn Hàn Quốc"}
+                    tlink={
+                      "https://img.tastykitchen.vn/resize/764x-/2022/04/15/cach-lam-trung-cuon-han-quoc-01-62e3.png"
+                    }
+                  ></DisplayItem>
+                </div>
+                <div className="display_item2 ">
+                  <DisplayItem
+                    istrue={true}
+                    ttime={140}
+                    ttag={"Vietnamese Food"}
+                    tby={"Admin"}
+                    tcomments={65}
+                    tname={"Trứng cuộn Hàn Quốc"}
+                    tlink={
+                      "https://img.tastykitchen.vn/resize/764x-/2022/04/15/cach-lam-trung-cuon-han-quoc-01-62e3.png"
+                    }
+                  ></DisplayItem>
+                </div>
+              </div>
+              <div className="display_row">
+                <div className="display_item1">
+                  <DisplayItem
+                    istrue={true}
+                    ttime={140}
+                    ttag={"Vietnamese Food"}
+                    tby={"Admin"}
+                    tcomments={65}
+                    tname={"Trứng cuộn Hàn Quốc"}
+                    tlink={
+                      "https://img.tastykitchen.vn/resize/764x-/2022/04/15/cach-lam-trung-cuon-han-quoc-01-62e3.png"
+                    }
+                  ></DisplayItem>
+                </div>
+                <div className="display_item2 ">
+                  <DisplayItem
+                    istrue={true}
+                    ttime={140}
+                    ttag={"Vietnamese Food"}
+                    tby={"Admin"}
+                    tcomments={65}
+                    tname={"Trứng cuộn Hàn Quốc"}
+                    tlink={
+                      "https://img.tastykitchen.vn/resize/764x-/2022/04/15/cach-lam-trung-cuon-han-quoc-01-62e3.png"
+                    }
+                  ></DisplayItem>
+                </div>
+              </div>
+              <div className="display_row">
+                <div className="display_item1">
+                  <DisplayItem
+                    istrue={true}
+                    ttime={140}
+                    ttag={"Vietnamese Food"}
+                    tby={"Admin"}
+                    tcomments={65}
+                    tname={"Trứng cuộn Hàn Quốc"}
+                    tlink={
+                      "https://img.tastykitchen.vn/resize/764x-/2022/04/15/cach-lam-trung-cuon-han-quoc-01-62e3.png"
+                    }
+                  ></DisplayItem>
+                </div>
+                <div className="display_item2 ">
+                  <DisplayItem
+                    istrue={true}
+                    ttime={140}
+                    ttag={"Vietnamese Food"}
+                    tby={"Admin"}
+                    tcomments={65}
+                    tname={"Trứng cuộn Hàn Quốc"}
+                    tlink={
+                      "https://img.tastykitchen.vn/resize/764x-/2022/04/15/cach-lam-trung-cuon-han-quoc-01-62e3.png"
+                    }
+                  ></DisplayItem>
+                </div>
+              </div>
+              <div className="display_row">
+                <div className="display_item1">
+                  <DisplayItem
+                    istrue={true}
+                    ttime={140}
+                    ttag={"Vietnamese Food"}
+                    tby={"Admin"}
+                    tcomments={65}
+                    tname={"Trứng cuộn Hàn Quốc"}
+                    tlink={
+                      "https://img.tastykitchen.vn/resize/764x-/2022/04/15/cach-lam-trung-cuon-han-quoc-01-62e3.png"
+                    }
+                  ></DisplayItem>
+                </div>
+                <div className="display_item2 ">
+                  <DisplayItem
+                    istrue={true}
+                    ttime={140}
+                    ttag={"Vietnamese Food"}
+                    tby={"Admin"}
+                    tcomments={65}
+                    tname={"Trứng cuộn Hàn Quốc"}
+                    tlink={
+                      "https://img.tastykitchen.vn/resize/764x-/2022/04/15/cach-lam-trung-cuon-han-quoc-01-62e3.png"
+                    }
+                  ></DisplayItem>
+                </div>
+              </div>
+              <br />
+              <Pagination
+                align="center"
+                defaultCurrent={1}
+                total={600}
+                className="pagination"
+              />
+              <br />
+              <br />
             </div>
           </div>
         </div>
