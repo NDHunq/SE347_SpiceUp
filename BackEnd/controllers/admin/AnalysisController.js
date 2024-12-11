19063939eee9a9d619bbea95f3c32f32c8ece906
Baonguyen1711 <128447276+Baonguyen1711@users.noreply.github.com>
2024-12-11 23:10:14 +0700

    modify analys to take year only

--- BackEnd/controllers/admin/AnalysisController.js
+++ BackEnd/controllers/admin/AnalysisController.js
@@ -8,104 +8,103 @@ const Product = require('../../models/Product')
 class AnalysisController {
     async getAnalysis(req, res) {
         try {
-            await connectToDb()
-            const { month, year } = req.query
-
+            await connectToDb();
+            const { month, year } = req.query;
+    
             const filterByDate = (month, year, fieldName) => {
+                const yearCondition = {
+                    '$eq': [
+                        { '$year': '$' + fieldName },
+                        parseInt(year)
+                    ]
+                };
+    
+                // Include month condition only if the month is provided
+                const conditions = month
+                    ? [
+                        yearCondition,
+                        {
+                            '$eq': [
+                                { '$month': '$' + fieldName },
+                                parseInt(month)
+                            ]
+                        }
+                    ]
+                    : [yearCondition];
+    
                 return {
                     '$match': {
                         '$expr': {
-                            '$and': [
-                                {
-                                    '$eq': [
-                                        {
-                                            '$month': '$' + fieldName
-                                        }, parseInt(month)
-                                    ]
-                                }, {
-                                    '$eq': [
-                                        {
-                                            '$year': '$' + fieldName
-                                        }, parseInt(year)
-                                    ]
-                                }
-                            ]
+                            '$and': conditions
                         }
                     }
-                }
+                };
             };
-
-            const totalSum = (fieldName) => {
-                return {
-                    '$group': {
-                        _id: null,
-                        totalSum: { '$sum': '$' + fieldName }
-                    }
+    
+            const totalSum = (fieldName) => ({
+                '$group': {
+                    _id: null,
+                    totalSum: { '$sum': '$' + fieldName }
                 }
-            }
-
-            const unWind = (fieldName) => {
-                return {
-                    '$unwind': '$' + fieldName
-                }
-            }
-
+            });
+    
+            const unWind = (fieldName) => ({
+                '$unwind': '$' + fieldName
+            });
+    
             const numberOfCustomerArray = await User.aggregate([
                 filterByDate(month, year, 'createdAt')
             ]);
-
-
+    
             const numberOfCustomer = numberOfCustomerArray.length > 0 ? numberOfCustomerArray.length : 0;
-
+    
             const numberOfSoldOutArray = await OrderItem.aggregate([
-                [
-                    filterByDate(month, year, 'dateOrdered'),
-                    unWind('orderItems'),
-                    totalSum('orderItems.quantities')
-                ]
+                filterByDate(month, year, 'dateOrdered'),
+                unWind('orderItems'),
+                totalSum('orderItems.quantities')
             ]);
-
+    
             const numberOfSoldOut = numberOfSoldOutArray.length > 0 ? numberOfSoldOutArray[0].totalSum : 0;
-
+    
             const profitArray = await Order.aggregate([
                 filterByDate(month, year, 'dateOrdered'),
                 totalSum('totalCost')
             ]);
-
+    
             const profit = profitArray.length > 0 ? profitArray[0].totalSum : 0;
-
+    
             const viewsArray = await Recipe.aggregate([
                 filterByDate(month, year, 'createdAt'),
                 totalSum('views')
             ]);
-
+    
             const views = viewsArray.length > 0 ? viewsArray[0].totalSum : 0;
-
+    
             const numberOfRecipeArray = await Recipe.aggregate([
                 filterByDate(month, year, 'createdAt')
             ]);
-
+    
             const numberOfRecipe = numberOfRecipeArray.length > 0 ? numberOfRecipeArray.length : 0;
-
+    
             const numberOfProductArray = await Product.aggregate([
                 filterByDate(month, year, 'date_ordered')
             ]);
-
+    
             const numberOfProduct = numberOfProductArray.length > 0 ? numberOfProductArray.length : 0;
-
+    
             res.status(200).json({
                 totalCustomers: numberOfCustomer,
                 profit: profit,
                 productsSoldOut: numberOfSoldOut,
-                recipeVỉews: views,
+                recipeViews: views,
                 totalRecipes: numberOfRecipe,
                 totalProducts: numberOfProduct
-            })
-
+            });
+    
         } catch (e) {
-            console.log("Some errors happen", e)
+            console.log("Some errors happen", e);
+            res.status(500).send("Internal Server Error");
         }
-
     }
 }
 
